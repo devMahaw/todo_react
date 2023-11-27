bbfc89513e5dfdd45f8b2c769548304d4a8ae720
Marcelo Dantas <mahawprogramacao@gmail.com>
2023-11-27 14:38:29 -0300

    feat: parameterizing filters

--- src/containers/sidebar/index.tsx
+++ src/containers/sidebar/index.tsx
@@ -7,12 +7,12 @@ const Sidebar = () => (
     <div>
       <S.Search type="text" placeholder="Buscar" />
       <S.Filters>
-        <CardFilter />
-        <CardFilter />
-        <CardFilter />
-        <CardFilter />
-        <CardFilter />
-        <CardFilter active />
+        <CardFilter subtitle="pendentes" counter={1} />
+        <CardFilter subtitle="concluídas" counter={2} />
+        <CardFilter subtitle="urgentes" counter={3} />
+        <CardFilter subtitle="importantes" counter={4} />
+        <CardFilter subtitle="normal" counter={5} />
+        <CardFilter subtitle="todas" counter={10} active />
       </S.Filters>
     </div>
   </S.Aside>
